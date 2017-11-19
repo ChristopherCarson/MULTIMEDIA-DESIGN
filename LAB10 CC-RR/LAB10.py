a0fa54187fe220e86e0375326c398f484050d0d0
granolavsraosins <33336533+granolavsraosins@users.noreply.github.com>
2017-11-19 01:07:06 -0800

    lab 10 and zip for lab 8

--- LAB10 CC-RR/LAB10.py
+++ LAB10 CC-RR/LAB10.py
@@ -1,2 +1,15 @@
-def hangman():
-  Let's do this...
+#CST205 Chris Carson and Raul Ramirez LAB 10
+
+#Warm Up PART A: Try out the new requestString() function. 
+#In JES, type the following command
+#name = requestString("What is your name?")
+#Now print the value of the variable name.
+def warmUp():
+  name = requestString("What is your name?")
+  print name.title()#makes any name input capitalized in the first letter 
+  #PART B: Write a while loop that continually asks the user to 
+  #enter a word (using request string).The loop should keep 
+  #going until the user enters the word "stop". 
+  while(name.lower() != "stop"):#will make any input lower case       
+    print name.title()
+    name = requestString("type stop to end loop")
