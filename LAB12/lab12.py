0d510e1a934f7de594bc7b37634fe49e14f942df
mrose2611 <33336494+mrose2611@users.noreply.github.com>
2017-11-26 21:16:23 -0800

    Added Raul's descriptions and added lab 12 folder with identical code
    Added Raul's descriptions to finish lab 11 (sorry if I've overstepped).  Added lab 12 folder with the same code but with added functionality.  There is a knife in the dining you can take and if you go to the front porch again you will lose.

--- LAB12/lab12.py
+++ LAB12/lab12.py
@@ -1,5 +1,5 @@
-#Lab 11
-
+#Lab 12
+#Team Code Blooded
 import time
 
 #Class for creating an object that represents a room in a text-based adventure game.
@@ -66,7 +66,7 @@ class Player:
     
     def __init__(self, location):
       self.location = location #defines starting room. Location parameter requires room object.
-      self.inventory = {} #defines the player's inventory.
+      self.inventory = [] #defines the player's inventory.
 
     #Moves the player object to another room if the room has a connected room in the provided direction.   
     def move(self, direction):
@@ -89,7 +89,7 @@ What happened to my beer?  To the east is the door back to the entrance of the h
 d4 = """You enter the front of the house. The lights are off and for now, it is almost completely quiet. The faint sounds of a nearby clock's internal mechanisms are 
 the only noise. To the west, the door to the patio is slightly ajar and a mild breeze is coming through. To the north lies the dining area."""
 d5 = """Upon the entering the dining room, a sour, stale stench hits your face. What is that? Has some foul smelling creature been in here? Or do I just need to take 
-out the gargbage? You look around the room and notice what appear to be smudgy tracks leading outside throug the North door of the dining area."""
+out the gargbage? You look around the room and notice what appear to be smudgy tracks leading outside throug the North door of the dining area. There is a bloody knife on the table. You might be able to use it."""
 d6 = """You enter the gym.  You are immediately hit with the smell of musty towels.  From the west wall you hear the faint humming sound of a treadmill.  
 Could someone have left it on?  Or did I forget to turn it off?  Near the dumbbell rack to the north is a door leading outside.  To the east is a door with a window that leads outside.  
 You can see the pool house and the door leading to the dining area through it."""
@@ -108,13 +108,17 @@ little kid. No one else would believe you, but you know what you saw. Those terr
 d12 = """You approach a chain-link fence. The fence is in disrepair and no longer stands straight, but is still managing to do its job. 
 In the distance over the fence, you can almost make out what appears to be a better game. Unfortunately, you have no way to get to it."""
 
+#Alternate Room descriptions
+alt5 = """Upon the entering the dining room, a sour, stale stench hits your face. What is that? Has some foul smelling creature been in here? Or do I just need to take 
+out the gargbage? You look around the room and notice what appear to be smudgy tracks leading outside throug the North door of the dining area. 
+There was a knife on the table, but now you have it in your hand.  You should probably avoid going back to the front of the house."""
 #Game begins here.
 #Initiate the 12 room object instances 
 r1 = Room("Front Porch", d1)
 r2 = Room("East Fence", d2)
 r3 = Room("Covered Patio", d3)
 r4 = Room("Inside House", d4)
-r5 = Room("Dining Area", d5)
+r5 = Room("Dining Area", d5, "knife")
 r6 = Room("Gym", d6)
 r7 = Room("Outside South", d7)
 r8 = Room("Outside West", d8)
@@ -142,8 +146,18 @@ def printIntro():
   printNow("""Welcome to The Code Blooded House of Horror!
 While in each room, you will be told which direction you can move. You can move in that direction by typing N for North, E for East, W for West and S for South.
 Type help to return to these instruction anytime. Type exit to quit the game.\n""")
-  time.sleep(5)
-  
+  time.sleep(1)
+
+#takes an object from a room if applicable and adds it to player inventory
+def takeObject(room, player):
+  if room.inventory =="knife":
+    player.inventory.append("knife")
+    room.inventory = ""
+    room.description = alt5
+    printNow("You take the bloody knife from the table to use later just in case.")
+  elif room.inventory == "":
+    printNow("There is nothing you can take in this room")
+
 def useObject(room):
   if room.inventory == "":
     printNow("There is nothing to use in this room.")
@@ -153,26 +167,39 @@ def useObject(room):
 #Initate the player object.
 player = Player(r1)
 end = false #Variable to flag whether the player wishes to end the game and exit.
+lose = false #Variable for the lose condition
 correctInput = "NSEW"
 
 printIntro()
 
+player.inventory
 #Main while loop.
-while end == false:
+while end == false and lose == false:
   printNow(player.location.outString())
   command = requestString("What direction would you like to move?\nN = North\nE = East\nW = West\nS = South\n"+
-  "'use' = use item in room\n'help' = instructions\n'exit' = quite game")
+  "'use' = use item in room\n'take' = take an item from the room\n'help' = instructions\n'exit' = quit game")
   if command.upper() == "EXIT":
     end = true
   elif command.upper() == "HELP":
     printIntro()
   elif command.upper() == "USE":
     useObject(player.location)
+  elif command.upper() == "TAKE":
+    takeObject(player.location, player)
   elif command.upper() in correctInput:
     player.move(command)
   else:
     printNow("I'm sorry, that's not a correct command... Please try again")
+  if "knife" in player.inventory and player.location == r1:#The player loses if they take the knife and go to the front porch
+    lose = true
+    break; 
   
-printNow("No one likes a quitter.")
+   
+#end while loop check for conditions
+if lose:
+  printNow("As you return to the front porch you are tackled by a passerby who sees the knife in your hands and mistakes you for a serial killer."+ 
+  "He pins you down and calls the police. You are arrested and sent to jail.\n YOU LOSE.")
+else:
+  printNow("No one likes a quitter.")
 
 
