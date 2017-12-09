035498603cf3c58f50ba2f834da1de68922ba6c6
CodeBloodedChris <33336510+CodeBloodedChris@users.noreply.github.com>
2017-12-09 11:36:26 -0800

    Updated Craps Game

--- LAB15 CC-RR/LAB15_Carson_Ramirez.py
+++ LAB15 CC-RR/LAB15_Carson_Ramirez.py
@@ -1,33 +1,43 @@
 #CST 205 lab15 problem 1 Christopher Carson, Raul Ramirez  
 from random import randint
 
-def roll():#rolls two dice
-  dice = randint(1,6)+randint(1,6)#adds random dice values
-  print(dice)
+def roll():#rolls one dice
+  diceGraphics = ['[ . ]', '[ : ]', '[ .:]','[ ::]','[.::]','[:::]']
+  dice = randint(1,6)
+  print(diceGraphics[dice-1])
   return dice
   
 def craps():
+  end = false
   wannaPlay = requestString("This is craps, if your first roll is " + 
-  "7 or 11 you win. If it's 2, 3 or 12 you lose. You keep rolling " +
-  "until you get the same number and win or roll a 7 and lose.\n" +
+  "7 or 11 you win.\n If it's 2, 3 or 12 you lose.\n Any other number is your point number.\n You keep rolling " +
+  "until you get the point again and win or roll a 7 and lose.\n\n" +
   " Type y/n to play")
   if wannaPlay.upper() == "Y":
-    #dice are rolled until point is rolled twice for win or roll 7 for lose. 
-    while True:    
-      firstR = roll()#first roll
-      if firstR in (2,3,12):#If first roll is 2,3,12 then lose!
-        print "You Lose!"
-        break;
-      elif firstR in (7,11):#if first roll is a 7 or 11 then win!
-        print "You Win!"
-        break;
-      secondR = roll()#send roll if first roll isn't a win
-      if secondR == firstR:#this would be point being rolled twice
-        print "You Win!"
-        break;
-      if secondR == 7:
-        print "You Lose!"
-        break;
+    #First Roll
+    count = roll()
+    count += roll()
+    point = count
+    
+    print "You rolled %s." % (count)
+    if count in (2,3,12):#If first roll is 2,3,12 then lose!
+      print "You Lose!"
+      end = true
+    elif count in (7,11):#if first roll is a 7 or 11 then win!
+      print "You Win!"
+      end = true
+
+    while end == false:
+      print "You roll again..."
+      count = roll()
+      count += roll()
+      if count == point:
+        print "You rolled your point again! You win!"
+        end = true
+      elif count == 7:
+        print "You loose!"
+        end = true
+
   elif wannaPlay.upper() == "N":
     showInformation("Ok go play something else then.")
   else:
