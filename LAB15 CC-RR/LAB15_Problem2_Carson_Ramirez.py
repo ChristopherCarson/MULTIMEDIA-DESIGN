dba47a29a85a7aff93c9bf75ae2be1ca045d2bb6
CodeBloodedChris <33336510+CodeBloodedChris@users.noreply.github.com>
2017-12-09 12:08:55 -0800

    Updated Problem 2

--- LAB15 CC-RR/LAB15_Problem2_Carson_Ramirez.py
+++ LAB15 CC-RR/LAB15_Problem2_Carson_Ramirez.py
@@ -3,20 +3,25 @@
 #get python to print days until next birth day
 #get python to print day of the week the Declaration was ratified.
 import datetime
+import calendar
 
-mydate = datetime.date(1989,02,24)#prints out month I was born
-print(mydate.strftime("%B"))#formats it to the month
+showInformation("First we need to get your birthdate.")
+y = int(requestString("Input the year you were born as a number: "))
+m = int(requestString("Input the month you were born as a number: "))
+d = int(requestString("Input the day you were born as a number: "))
 
-today = datetime.date.today()#takes today
-next = datetime.date(2018,02,24)#takes birthday
-diff = next - today#calculates how many days
-print diff.days
+#Print a calendar of the month you were born
+print(calendar.month(y, m))
 
-def olderDate():
-  from datetime import datetime
+#Print the number of days until your next birthday
+today = datetime.date.today()
+if datetime.date(today.year,m,d) < today:
+  diff = datetime.date(today.year+1,m,d) - today
+  print("There are %s days until your next birthday.") % (diff.days)
+else:
+  diff = datetime.date(today.year,m,d) - today
+  print("There are %s days until your next birthday.") % (diff.days)
 
-  mDt = datetime(1900,01,01)
-  dt = datetime.strptime('20-02-1899', "%d-%m-%Y")
-  resultString = datetime(dt.year + (mDt - dt).days/365 + 
-  1, dt.month, dt.day).strftime('%B %d, %Y').replace('1900', str(dt.year))
-  print resultString
+#Print the name of the weekday Congress ratified Dec. of Ind.
+date = datetime.date(1776, 7, 4)
+print("The Declaration of Independence was ratified by Congress on a %s." )% (calendar.day_name[date.weekday()])
