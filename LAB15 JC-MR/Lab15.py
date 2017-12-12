1e33fa5a63f128bfd1fa1002709015a3de8ecdd4
John Coffelt <33336503+JohnRCoffelt@users.noreply.github.com>
2017-12-11 20:56:07 -0800

    Added problem 1

--- LAB15 JC-MR/Lab15.py
+++ LAB15 JC-MR/Lab15.py
@@ -1,11 +1,51 @@
 #Lab 15
 import calendar
 import datetime
-#Note the lab name and partner name have to be formatted as stated in the assignment.  I would delete this comment after formatting it.
-#Partner: 
+#Partner: John Coffelt, Michael Rose
+
+#Problem 1
 
 #Problem 2
+#Rolls a "dice". Takes the amount of sides a dice has and then rolls a random number and returns it.
+def roll_dice(num_sides):
+  import random
+  return random.randint(1,num_sides)
 
+#Set up variables.    
+sides = 6
+point = 0
+
+#Set up conditional booleans.
+first_roll = true
+win = false
+game_over = false
+print "Welcome to Craps!"
+while not game_over:
+  raw_input("Enter any key to roll: ")
+  roll = roll_dice(sides) + roll_dice(sides)
+  print "You roll: " + str(roll)
+  
+  #Determine if the game is over based on the result of the roll.
+  if first_roll and roll in [7,11]:
+    win = true
+    game_over = true
+  elif first_roll and roll in [2,3,12]:
+    game_over = true 
+  elif first_roll and roll in [4,5,6,8,9,10]:
+    point = roll
+  elif roll == point:
+    win = true
+    game_over = true
+  elif roll == 7:
+    game_over = true
+    
+  if first_roll: first_roll = false
+  
+if win:
+  print "Congratulations! You won!"
+else:
+  print "Sorry! You lost!"
+  
 #This function takes a year and a month and prints out the calendar for the month in that year.
 #Use a monospaced font to remove formatting issues
 def birthMonth(year, month):
